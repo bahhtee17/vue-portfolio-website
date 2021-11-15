9bfe3f762d13e9b672019b9213b87483b50ce7e7
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-11-15 23:01:30 +0500

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="home" to="/">Home</router-link>
+      <router-link class="about" to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -12,7 +12,49 @@
 body{
   padding: 0;
   margin: 0;
+}
 
+#app #nav{
+  font-family: Roboto;
+ background-color: #A6998A;
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  ;
+  padding-left:3%;
+  text-decoration: none;
+}
+
+.home{
+
+  text-decoration: none;
+  font-size: 2vw;
+  color:white
+}
+
+.home:hover{
+color: #ffffcd;
+}
+
+.about{
+  font-size: 2vw;
+  text-decoration: none;
+  color:white
+}
+
+.about:hover{
+  color: #ffffcd;
+}
+
+@media only screen and (max-width: 600px){
+  .home{
+    font-size: 3vw;
+  }
+
+  .about{
+    font-size: 3vw;
+  }
 }
 
 
