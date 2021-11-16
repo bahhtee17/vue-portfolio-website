d5ad68c1f0ad96d102a7cd40931be03a9a363903
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-11-17 01:12:18 +0500

    sectionOne

---
+++ src/components/FirstSection.vue
@@ -0,0 +1,53 @@
+<template>
+ <div class="container">
+
+    <div class="img-container">
+        <img class="image" src="../assets/images/section_01.jpg"/>
+    </div>
+
+    <div class="text">
+       <h3>Tandem Skydive</h3>
+       <p> For the spirited adrenaline junkie, there really is no greater challenge. This tandem skydive is the ultimate test of bravery for all thrill seekers, as you plummet from the plane. After your short briefing you will be kitted up in a jumpsuit ready to board the plane. An anxious 25-minute flight to reach the jump height will give you all the time you need to test your nerves to their limits, before you’re finally ready to take that leap of faith. Once harnessed to your fully qualified tandem partner, there is no going back as you fall through the sky, reaching speeds of 120mph. A truly unique sensation! Once you reach around 5000ft the instructor will deploy the parachute and you’ll float back down to solid ground, completing a memory that will stay with you for the rest of your life.</p>
+    </div>
+
+
+ </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    width: 100%;
+    font-size: 60%;
+     height: 40vw;
+    justify-content: center;
+    color: black;
+    margin-top: 5%;
+
+
+}
+
+
+
+
+ .img-container .image{
+  height: 90%;
+  background-size: cover;
+}
+
+.text{
+    text-align: center;
+     height: 90%;
+    letter-spacing: 0.1em;
+    line-height: 1.5em;
+    background-color: aliceblue;
+
+}
+</style>
